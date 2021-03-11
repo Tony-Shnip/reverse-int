4f94ee5126266c8db62ae2b4539c459e421baaaa
Tony-Shnip <shnip12345@gmail.com>
2021-03-11 19:59:35 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  if(n<0)n *= -1;
+  let str = String(n);
+  let ans = str[str.length-1];
+  for(let i = str.length - 2; i>=0; i--)
+  {
+      ans += str[i];
+  }
+  return ans;
 }
